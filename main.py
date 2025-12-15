f19f516f255765ecaa4b4da09eb0e4e7e5b6eb2f
FatihMakes <faitheditsx@gmail.com>
2025-12-15 18:58:00 +0300

    Update main.py

--- main.py
+++ main.py
@@ -2,8 +2,8 @@ import asyncio
 import threading
 from voice_player import VoiceImagePlayer, edge_speak
 from speech_to_text import record_voice
-#from recognition import record_voice
 from groq_ai import get_response
+from aircraft_module import handle_aircraft_command
 
 async def get_voice_input():
     return await asyncio.to_thread(record_voice)
@@ -15,26 +15,18 @@ async def ai_loop(player):
         if not user_text:
             continue
 
-        print("User:", user_text)
-        lowered = user_text.lower().strip()
-
-        if lowered in ["exit", "quit", "goodbye", "bye", "stop","mute"]:
-            edge_speak("Goodbye sir.", player)
-            player.root.after(300, player.root.destroy)
-            break
+        handled = handle_aircraft_command(user_text, player)
+        if handled:
+            continue
 
         response = get_response(user_text)
         print("AI:", response)
-
+        
         edge_speak(response, player)
-
         player.write_log(f"You: {user_text}")
         player.write_log(f"AI: {response}")
-
-
+    
 def main_loop():
-    print("")
-
     player = VoiceImagePlayer("face.png", size=(900, 900))
 
     def thread_target():
@@ -42,6 +34,7 @@ def main_loop():
 
     threading.Thread(target=thread_target, daemon=True).start()
     player.root.mainloop()
+
 
 if __name__ == "__main__":
     main_loop()
